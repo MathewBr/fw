fcbb516cf1474cf6bd726f4e46dd81f1a48dcdf8
Mathew Br <matvei_berezin@mail.ru>
2022-09-25 20:45:37 +0300

    part3 ex3 add admin gomepage widgets

--- app/controllers/admin/MainController.php
+++ app/controllers/admin/MainController.php
@@ -7,7 +7,12 @@ use fw\App;
 class MainController extends AppFeature{
 
     public function indexAction(){
+        $countNewOrders = \R::count('order', "status = '0'");
+        $countUsers = \R::count('user');
+        $countProducts = \R::count('product');
+        $countCategories = \R::count('category');
         $this->setMeta('Панель управления');
+        $this->passData(compact('countNewOrders', 'countUsers', 'countProducts', 'countCategories'));
     }
 
 }
